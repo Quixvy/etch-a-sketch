786f7e2f460bc497e8b05360bcd91ade457edd25
Quixvy <tjpareja330@gmail.com>
2026-05-26 10:54:15 +0800

    feat: Move the RGB generator to the function and in loop

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const container = document.querySelector(".container");
 const btnGenerator = document.querySelector(".btn_gen_grid");
+
 btnGenerator.addEventListener('click', () => {
     gridVal = prompt("Enter a value of boxes");
     if (gridVal <= 100) {
-         container.innerHTML = "";
+        container.innerHTML = "";
         makeGrid(gridVal);
     } else {
         alert("MAXIMUM OF BOXES IS ONLY 100!");
     }
-   
 }) 
-
 
 function getRandomRGB() {
     const r = Math.floor(Math.random() * 256);
@@ -20,33 +19,28 @@ function getRandomRGB() {
 }
 
 function makeGrid(size) {
-    const boxSize = 500  / size; 
-    for (let i = 0; i<size; i++) {
-        for (let y = 0; y<size; y++) {
+    const boxSize = 500 / size; 
+    for (let i = 0; i < size; i++) {
+        for (let y = 0; y < size; y++) {
             const div = document.createElement("div");
             div.classList.add("box");
             div.style.width = `${boxSize}px`;
             div.style.height = `${boxSize}px`;
-            
-
             div.addEventListener('mouseover', () => {
                 div.style.backgroundColor = getRandomRGB();
             })
-            container.addEventListener('touchmove', (e) => {
-            e.preventDefault();
-            const touch = e.touches[0];
-            const element = document.elementFromPoint(touch.clientX, touch.clientY);
-            if (element.classList.contains('box')) {
-                element.style.backgroundColor = getRandomRGB();
-            }
-            })
-
             container.appendChild(div); 
         }
     }
 }
 
+container.addEventListener('touchmove', (e) => {
+    e.preventDefault();
+    const touch = e.touches[0];
+    const element = document.elementFromPoint(touch.clientX, touch.clientY);
+    if (element.classList.contains('box')) {
+        element.style.backgroundColor = getRandomRGB();
+    }
+})
 
 makeGrid(50)
-
-
